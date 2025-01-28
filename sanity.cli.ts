22e1ed57358226a54a73d6c8b43e5d49dc27447b
Michael Parque <michael.t.parque@outlook.com>
2025-01-28 16:33:03 -0500

    build: set studioHost = 'bm-news'
    This sets the deployment url for `pnpm run deploy` that gets hosted by
    sanity at https://bm-news.sanity.studio/

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,10 +1,11 @@
-import {defineCliConfig} from 'sanity/cli'
+import { defineCliConfig } from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
     projectId: 'hly7cmhd',
     dataset: 'production'
   },
+  studioHost: 'bm-news',
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
